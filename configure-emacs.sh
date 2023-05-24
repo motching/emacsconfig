457eb222bb7c8a7db556597d649a4397abf40c46
Attila Kovacs <akovacs101386@gmail.com>
2023-05-23 18:26:27 -0700

    Remember configure flags

---
+++ configure-emacs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Just so I remember the configure flags.
+# To be run from the main emacs folder.
+# For 29.0.90, later this stuff might become obsolete.
+./configure --with-native-compilation --with-json --with-imagemagick --with-xwidgets
